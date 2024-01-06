3f7fce97d91233a881b855da43f52f2013f8eb88
ShlomoCode <78599753+ShlomoCode@users.noreply.github.com>
2024-01-07 00:24:04 +0200

    support app icon

---
+++ create_mipmap_images.js
@@ -0,0 +1,27 @@
+const Jimp = require('jimp');
+
+const sizes = {
+    xxxhdpi: 192,
+    xxhdpi: 144,
+    xhdpi: 96,
+    hdpi: 72,
+    mdpi: 48,
+    ldpi: 36,
+};
+
+async function main(imageUrl) {
+    const image = await Jimp.read(imageUrl);
+    image.resize(512, 512).write('app/src/main/ic_launcher-playstore.png');
+    for (const [sizeName, sizePixels] of Object.entries(sizes)) {
+        const launcherPath = `app/src/main/res/mipmap-${sizeName}/ic_launcher.png`;
+        const roundPath = `app/src/main/res/mipmap-${sizeName}/ic_launcher_round.png`;
+        image.resize(sizePixels, sizePixels).write(launcherPath);
+        image.circle().write(roundPath);
+    }
+}
+
+if (require.main === module) {
+    const arg = process.argv[2];
+    if (!arg) throw new Error('missing argument');
+    main(arg);
+}
